32ac0f779f5e0c39cd515058114e89a59d9d8b25
JubairRafi <jubairpial96960@gmail.com>
2020-12-19 04:16:32 +0600

    user model added

--- laratest/app/Http/Controllers/loginController.php
+++ laratest/app/Http/Controllers/loginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request; 
+use App\Models\user;
 
 class loginController extends Controller
 {
@@ -10,14 +11,17 @@ class loginController extends Controller
        return view('login.index');
    }
    public function verifyy(Request $req){
-    if($req->username == $req->password){
-        $req->session()->put('username', $req->username);
-        $req->session()->put('type', $req->username);
-        return redirect()->route('home.index');
-    }else{
-        $req->session()->flash('msg', 'invalid username/password');
-        return redirect('/login');
-        //return view('login.index');
-    }
+       $users=user::all();
+
+       print_r($users);
+    // if($req->username == $req->password){
+    //     $req->session()->put('username', $req->username);
+    //     $req->session()->put('type', $req->username);
+    //     return redirect()->route('home.index');
+    // }else{
+    //     $req->session()->flash('msg', 'invalid username/password');
+    //     return redirect('/login');
+    //     //return view('login.index');
+    // }
    }
 }
